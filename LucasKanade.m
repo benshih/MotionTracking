2441bdea9b15345503304fec427eaf690b4ef703
Benjamin Shih <benjshih@gmail.com>
2013-10-22 13:14:31 -0400

    initial header file for LK function

---
+++ LucasKanade.m
@@ -0,0 +1,19 @@
+% Benjamin Shih
+% 16720f13 Computer Vision
+% Assignment 4 Tracking
+% 10/22/2013
+% 1.2 Lucas-Kanade
+
+function [ u, v ] = LucasKanade( It, It1, rect )
+    % Top left corner
+    x1 = rect(1);
+    y1 = rect(2);
+    % Bottom right corner
+    x2 = rect(3);
+    y2 = rect(4);
+    
+    
+
+
+end
+
