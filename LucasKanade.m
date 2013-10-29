bbcdb07ad3bdee4527c28e18ca66fb4171f02a6a
Benjamin Shih <benjshih@gmail.com>
2013-10-29 17:51:17 -0400

    revamped LucasKanade tracker that doesn't use the paper algorithm, but instead uses the optimization formula presented in the hw documentation

--- LucasKanade.m
+++ LucasKanade.m
@@ -11,45 +11,63 @@ function [ u, v ] = LucasKanade( It, It1, rect )
     % Bottom right corner
     x2 = rect(3);
     y2 = rect(4);
+    [Itx, Ity, ~] = size(It); % same size as It1
     
+    [X, Y] = meshgrid(1:Itx, 1:Ity);
+    X = im2double(X');
+    Y = im2double(Y');
+    [Xq, Yq] = meshgrid(x1:x2, y1:y2);
+    % These values represent the interpolated intensities corresponding to
+    % values in the Xq, Yq meshgrid.
+    Vqt = interp2(X, Y, It, Xq, Yq);
+    Vqt1 = interp2(X, Y, It1, Xq, Yq);
+ 
+    % Make large matrix.
+    [ix, iy] = gradient(Vqt);
+    deltaI = Vqt(:) - Vqt1(:);
+    A = [ix(:) iy(:)];
     
-    % Initial warp.
-    warp = [0 0]';
+    % Compute the optimized solution for the minimization.
+    a = (A'*A)^-1*A'*deltaI;
     
-    epsilon = 0.03;
-    
-    Itcurr = It;
-    Itnext = It1;
-    
-    while (p > epsilon)
-        % 1. Warp It.
-        tform = affine2d([1 0 warp(1); 0 1 warp(2); 0 0 1]);
-        Itwarp = imwarp(Itnext, tform);
-        
-        % 2. Compute the error image T(x) - I(W(x;p))
-        template = Itcurr(x1:x2, y1:y2);
-        It_template = Itwarp(x1:x2, y1:y2);
-        
-        % 3. Warp the gradient with W(x;p)
-        [ix iy] = gradient(Itwarp);
-        ixNew = imwarp(ix, tform);
-        iyNew = imwarp(iy, tform);
-        
-        % 4. Evaluate the Jacobian at (x; p). In this case it's the identity
-        % matrix because we only have a transform. How to genralize this?
-        jacob = eye(2);
-        
-        % 5. Compute the steepest descent images.
-        ixSteep = ixNew * jacob;
-        iySteep = iyNew * jacob;
-        
-        % Compute the Hessian matrix.
-        steepJacob = [ixSteep; 
-    
-    % Compute the n x n (Gauss-Newton approximation to the) Hessian matrix.
-    H = gradient(It);
-    end
+    u = a(1);
+    v = a(2);
+end
 
 
 
-    % algorithm flow chart: http://www.cse.psu.edu/~rcollins/CSE598G/LKintroContinued.pdf
+% algorithm flow chart: http://www.cse.psu.edu/~rcollins/CSE598G/LKintroContinued.pdf
+
+%     while (p > epsilon)
+%         % 1. Warp It.
+%         tform = affine2d([1 0 warp(1); 0 1 warp(2); 0 0 1]);
+%         Itwarp = imwarp(Itnext, tform);
+%         
+%         % 2. Compute the error image T(x) - I(W(x;p))
+%         template = Itcurr(x1:x2, y1:y2);
+%         It_template = Itwarp(x1:x2, y1:y2);
+%         
+%         % 3. Warp the gradient with W(x;p)
+%         [ix iy] = gradient(Itwarp);
+%         ixNew = imwarp(ix, tform);
+%         iyNew = imwarp(iy, tform);
+%         
+%         % 4. Evaluate the Jacobian at (x; p). In this case it's the identity
+%         % matrix because we only have a transform. How to genralize this?
+%         jacob = eye(2);
+%         
+%         % 5. Compute the steepest descent images.
+%         ixSteep = ixNew * jacob;
+%         iySteep = iyNew * jacob;
+%         
+%         % 6. Compute the Hessian matrix.
+%         steepJacob = [ixSteep; iySteep];
+%         
+%         % 7. Compute the sum_x(gradientI*jacobian)'*(T(x) - I(W(x;p))).
+%         
+%         
+%         % 8. Compute change in parameters.
+%         
+%         
+%         % 9. Update the parameters.
+%     end
--- testCarSequence.m
+++ testCarSequence.m
@@ -4,3 +4,30 @@
 % 10/22/2013
 % 1.3 Track the speeding car
 
+% This variable "sequence" is of the dimension, h x w x m x n, where h and
+% w are the frame height and width, m (=3) is the number of color channel,
+% n is the number of frames.
+load('carSequence.mat');
+
+% Initial warp.
+deltaP = [0 0]';
+
+[h,w,m,n] = size(sequence);
+
+% Initial rect values.
+x1 = 328;
+y1 = 213;
+x2 = 419;
+y2 = 265;
+rect = [x1 y1 x2 y2];
+
+% rgb2gray for images because transpose of a m x n x k does not make sense
+
+for iFrame = 1:n
+    [u, v] = LucasKanade(It, It1, rect);
+    rect = [x1+u, y1+v, x2+u, y2+v];
+end
+
+
+
+% can use videowriter to record video
