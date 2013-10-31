9ec22a716b79a0fa22952665c50185e436e7811f
Benjamin Shih <benjshih@gmail.com>
2013-10-30 23:50:31 -0400

    working part 1

--- LucasKanade.m
+++ LucasKanade.m
@@ -8,38 +8,88 @@
 % convert images from rgb2gray and im2double before using, otherwise
 % interp2 gets angry
 
-function [ u, v ] = LucasKanade( It, It1, rect )
-    % Top left corner
-    x1 = rect(1);
-    y1 = rect(2);
-    % Bottom right corner
-    x2 = rect(3);
-    y2 = rect(4);
-    [Itx, Ity, ~] = size(It); % same size as It1
+function [u,v] = LucasKanade(It,It1,rect)
+    % Initialising u,v for motion estimate. 
+    u = 0;
+    v = 0;
+    % Initialising u1,v1 which are updates for u,v in each iteration
+    u1 = 0;
+    v1 = 0;
+    count = 0;
+
+    % Meshgrid for X,Y for the entire range of image
+%     [X,Y]=meshgrid(1:size(It,2),1:size(It,1));
     
-    [X, Y] = meshgrid(1:Itx, 1:Ity);
-    [Xq, Yq] = meshgrid(x1:x2, y1:y2);
-    % These values represent the interpolated intensities corresponding to
-    % values in the Xq, Yq meshgrid.
-    Vqt = interp2(X, Y, It', Xq, Yq);
-    Vqt1 = interp2(X, Y, It1', Xq, Yq);
- 
-    % Make large matrix.
-    [ix, iy] = gradient(Vqt1); % q: should this be on t or t+1?
-    deltaI = Vqt(:) - Vqt1(:);
-    A = [ix(:) iy(:)];
+    % Meshgrid for the coordinates in the template T
+    y=rect(2):rect(4);
+    x=rect(1):rect(3);
     
-    % Compute the optimized solution for the minimization.
-    a = (A'*A)^-1*A'*deltaI;
-    
-    u = a(1)
-    v = a(2)
+    % Calculating the values of the template using interpolation
+%     template=interp2(It,XI,YI);
+    template = It(y,x);
+
+    % Calculating the X and Y derivaties for the templates
+    [dX, dY] = gradient(double(template));
+%     template = template(y,x);
+
+    while((count < 50))
+        p=[u v];
+        count = count + 1;
+
+        % Map pixels in template to pixels in It1
+        % step 1
+    %     Xq1 = (X1)+p(1);
+    %     Yq1 = (Y1)+p(2);
+    %     [Xq, Yq] = meshgrid(Xq1,Yq1);
+    %     ItWarp = interp2(It1,Xq,Yq);
+    %     
+        ItWarp = warpImg(It1, rect, p);
+
+        % Find the difference between the warped image and the template. 
+        % step 2
+        deltaI = double(template-ItWarp);
+        deltaI = deltaI(:);
+
+        % Warp the gradients. step 3
+    %     dxWarp = interp2(dX,Xq,Yq); 
+    %     dyWarp = interp2(dY,Xq,Yq);
+        dxWarp = warpImg(dX, rect, p);
+        dyWarp = warpImg(dY, rect, p);
+        dxWarp = dxWarp(:);
+        dyWarp = dyWarp(:);
+
+        % Steepest descent images. step 5
+        warpedGrad = double([dX(:), dY(:)]);
+
+        % Compute the optimized solution for the minimization.
+        % step 6, 7, 8
+        deltaP = (warpedGrad'*warpedGrad)\(warpedGrad'*deltaI);
+
+        % step 9
+        u = p(1) + deltaP(1);
+        v = p(2) + deltaP(2);
+
+        if (norm(deltaP) < 0.01)
+            continue
+        end
+
+    end
+    count
+end
+
+function [warpedImg] = warpImg(img, rect, p)  
+    y=(rect(2)+p(2)):(rect(4)+p(2));
+    x=(rect(1)+p(1)):(rect(3)+p(1));
+    [X,Y]=meshgrid(x,y);
+
+%     [Xq, Yq] = meshgrid(X2,Y2);
+%     img = double(img);
+    warpedImg = interp2(img,X,Y);
 end
 
 
-
 % algorithm flow chart: http://www.cse.psu.edu/~rcollins/CSE598G/LKintroContinued.pdf
-
+% 
 %     while (p > epsilon)
 %         % 1. Warp It.
 %         tform = affine2d([1 0 warp(1); 0 1 warp(2); 0 0 1]);
--- testCarSequence.m
+++ testCarSequence.m
@@ -4,47 +4,34 @@
 % 10/22/2013
 % 1.3 Track the speeding car
 
+clc
 close all
 
-% This variable "sequence" is of the dimension, h x w x m x n, where h and
-% w are the frame height and width, m (=3) is the number of color channel,
-% n is the number of frames.
-load('carSequence.mat');
+load('carSequence.mat')
 
-% Initial warp.
-deltaP = [0 0]';
-
-[h,w,m,n] = size(sequence);
-
-% Initial rect values.
-x1 = 328;
-y1 = 213;
-x2 = 419;
-y2 = 265;
-rect = [x1 y1 x2 y2];
+rect=[328 213 419 265];
 
 width = abs(rect(1)-rect(3));
 height = abs(rect(2)-rect(4));
+coordinates=zeros(100,4);
 
+for i=1:100
+    img = im2double(sequence(:,:,:,i));
 
-figure;
-pause on;
-
-for iFrame = 1:n-1
-    Itcurr = rgb2gray(im2double(sequence(:,:,:,iFrame)));
-    Itnext = rgb2gray(im2double(sequence(:,:,:,iFrame+1)));
-    [u, v] = LucasKanade(Itcurr, Itnext, rect);
-    rect = [x1+u, y1+v, x2+u, y2+v]
-    
-    imshow(Itnext);
+    imshow(img);
     hold on;
     rectangle('Position',[rect(1),rect(2),width,height], 'LineWidth',2, 'EdgeColor', 'r')    
     hold off;
-    pause(0.0333); % 30 fps
+    pause(0.01); % 30 fps
+    
+    Itcurr = rgb2gray(im2double(sequence(:,:,:,i)));
+    Itnext = rgb2gray(im2double(sequence(:,:,:,i+1)));
+    [u,v] = LucasKanade(Itcurr,Itnext,rect)
+    rect=[rect(1)+u rect(2)+v rect(3)+u rect(4)+v];
+    coordinates(i,:) = rect;
+    %disp(['Frame ',num2str(i)]);
+
+    rect = round(coordinates(i,:))
 end
 
 close
-
-% bs notes:
-% rgb2gray for images because transpose of a m x n x k does not make sense
-% can use videowriter to record video
